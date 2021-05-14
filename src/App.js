37921aa151b975623e83a478d2d26ac1db52df06
Ana.Sousa <anasousa314@outlook.com>
2021-05-14 17:15:30 -0300

    adiciona App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Dash from './components/Dash';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Dash />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
