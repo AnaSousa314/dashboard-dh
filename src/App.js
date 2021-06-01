e9070132989c15c41aa664cae9af6463b01d6b9a
Ana.Sousa <anasousa314@outlook.com>
2021-06-01 15:52:23 -0300

    Altera App.js para ter o componente Footer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@
 import './App.css';
 import Content from './components/Content';
 import Dash from './components/Dash';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
   return (
+    <>
     <div className="App">
         <Dash />
         <main>
           <Header/>
           <Content/>
+          <Footer/>
         </main>
     </div>
+    </>
   );
 }
 
